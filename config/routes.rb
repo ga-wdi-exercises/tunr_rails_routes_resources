aad61a099bed4e64429fd670ed05d1241a52b9aa
Adrian Maseda <amaseda@gmail.com>
2015-07-28 17:09:06 -0400

    add commented-out individual routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,24 @@ Rails.application.routes.draw do
 
   resources :artists
   resources :songs
+
+  # # artists routes
+  # get     '/artists',          to: 'artists#index'
+  # get     '/artists/new',      to: 'artists#new'
+  # post    '/artists',          to: 'artists#create'
+  #
+  # get     '/artists/:id',      to: 'artists#show'
+  # get     '/artists/:id/edit', to: 'artists#edit'
+  # put     '/artists/:id',      to: 'artists#update'
+  # delete  '/artists/:id',      to: 'artists#destroy'
+  #
+  # # songs routes
+  # get     '/songs',          to: 'songs#index'
+  # get     '/songs/new',      to: 'songs#new'
+  # post    '/songs',          to: 'songs#create'
+  #
+  # get     '/songs/:id',      to: 'songs#show'
+  # get     '/songs/:id/edit', to: 'songs#edit'
+  # put     '/songs/:id',      to: 'songs#update'
+  # delete  '/songs/:id',      to: 'songs#destroy'
 end
