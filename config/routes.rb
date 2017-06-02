444ac04a101fda61e6533f9d35c8c4d5c5ed9369
Adrian Maseda <amaseda@gmail.com>
2017-06-02 14:21:44 -0400

    fix route indentation

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,11 @@ Rails.application.routes.draw do
   delete '/artists/:id'       => 'artists#destroy'
 
   # `resources :songs` generates the following routes
-  get    '/songs'           => 'songs#index'
-  get    '/songs/new'       => 'songs#new',   as: "new_song"
-  get    '/songs/:id'       => 'songs#show',  as: "song"
-  post   '/songs'           => 'songs#create'
-  get    '/songs/:id/edit'  => 'songs#edit',  as: "edit_song"
-  patch  '/songs/:id'       => 'songs#update'
-  delete '/songs/:id'       => 'songs#destroy'
+  get    '/songs'             => 'songs#index'
+  get    '/songs/new'         => 'songs#new',     as: "new_song"
+  get    '/songs/:id'         => 'songs#show',    as: "song"
+  post   '/songs'             => 'songs#create'
+  get    '/songs/:id/edit'    => 'songs#edit',    as: "edit_song"
+  patch  '/songs/:id'         => 'songs#update'
+  delete '/songs/:id'         => 'songs#destroy'
 end
