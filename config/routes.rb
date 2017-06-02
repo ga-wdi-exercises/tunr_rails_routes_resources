00bea99adce84923ccac9efc04ad862d6c3edc76
Adrian Maseda <amaseda@gmail.com>
2017-06-02 14:17:04 -0400

    fix route indentation

--- config/routes.rb
+++ config/routes.rb
@@ -6,19 +6,19 @@ Rails.application.routes.draw do
 
   # `resources :artists` generates the following routes
   get    '/artists'           => 'artists#index'
-  get    '/artists/new'       => 'artists#new', as: "new_artist"
-  get    '/artists/:id'       => 'artists#show', as: "artist"
+  get    '/artists/new'       => 'artists#new',   as: "new_artist"
+  get    '/artists/:id'       => 'artists#show',  as: "artist"
   post   '/artists'           => 'artists#create'
-  get    '/artists/:id/edit'  => 'artists#edit', as: "edit_artist"
-  patch    '/artists/:id'       => 'artists#update'
+  get    '/artists/:id/edit'  => 'artists#edit',  as: "edit_artist"
+  patch  '/artists/:id'       => 'artists#update'
   delete '/artists/:id'       => 'artists#destroy'
 
   # `resources :songs` generates the following routes
   get    '/songs'           => 'songs#index'
-  get    '/songs/new'       => 'songs#new', as: "new_song"
-  get    '/songs/:id'       => 'songs#show', as: "song"
+  get    '/songs/new'       => 'songs#new',   as: "new_song"
+  get    '/songs/:id'       => 'songs#show',  as: "song"
   post   '/songs'           => 'songs#create'
-  get    '/songs/:id/edit'  => 'songs#edit', as: "edit_song"
-  patch    '/songs/:id'       => 'songs#update'
+  get    '/songs/:id/edit'  => 'songs#edit',  as: "edit_song"
+  patch  '/songs/:id'       => 'songs#update'
   delete '/songs/:id'       => 'songs#destroy'
 end
